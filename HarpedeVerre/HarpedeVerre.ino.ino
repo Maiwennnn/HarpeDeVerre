3f1dd7f195b24f2e8e0e63c1af6e7678293a4620
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-03-02 15:10:51 +0100

    Dernière modif
    Ajout de note pour clavier, raccourci le basegain sur faust et le bow Pressure, enlever les commentaires et ajouter un peu d'interface dans le terminal

--- HarpedeVerre/HarpedeVerre.ino.ino
+++ HarpedeVerre/HarpedeVerre.ino.ino
@@ -16,23 +16,19 @@
 #include <SPI.h>
 #include <C:\Program Files (x86)\Arduino\hardware\teensy\avr\libraries\SD\src\SD.h>
 #include "glassHarp.h"
-#include "MyDsp.h"
-#include "AudioAnalyzePeak1.h"
 //---------------------------------------------------------------------------------------
 const int myInput = AUDIO_INPUT_MIC;
 
 //---------------------------------------------------------------------------------------
 AudioAnalyzeNoteFrequency notefreq;
-AudioPlayMemory           wav_note;
 AudioMixer4               mixer;
 AudioOutputI2S         audioOutput; 
 AudioInputI2S          audioInput;
-AudioAnalyzePeak1       aap1;
+AudioAnalyzePeak       aap1;
 glassHarp gh;
 float note_prec=10;
 float threshold =0.2; 
 float prevPeak=0;
-MyDsp d;
 boolean clavier=false;
 int incomingByte; 
 float freq; 
@@ -44,8 +40,6 @@ AudioConnection patchCord2(gh, 0, audioOutput, 0);
 AudioConnection patchCord3(gh, 0, audioOutput, 1);
 AudioControlSGTL5000 audioShield;
 //---------------------------------------------------------------------------------------
-//digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
-//---------------------------------------------------------------------------------------
 void setup() {
   Serial.begin(9600);
   AudioMemory(30);
@@ -53,70 +47,9 @@ void setup() {
   audioShield.inputSelect(myInput);
   audioShield.volume(0.8);
   mixer.gain(0,0.8);
-  //gh.setParamValue("bowPressure", 0.2); 
-  /*
-   *  Initialize the yin algorithm's absolute
-   *  threshold, this is good number.
-   */
   notefreq.begin(0.1);
   pinMode(A0, INPUT);
   pinMode(15, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void joueClavier(){
-  if (Serial.available() > 0) {
-    // read the oldest byte in the serial buffer:
-    incomingByte = Serial.read();
-    switch(incomingByte){
-      case 'a':
-        freq=261.6;
-        break;
-      case 'z':
-        freq=277.2;
-        break;
-      case 'e':
-        freq=293.7;
-        break;
-      case 'r':
-        freq=311.1;
-        break;
-      case 't':
-        freq=329.6;
-        break;
-      case 'y':
-        freq=349.2;
-        break;
-      case 'u':
-        freq=370;
-        break;
-      case 'i':
-        freq=392;
-        break;
-      case 'o':
-        freq=415.3;
-        break;
-      case 'p':
-        freq=440;
-        break;
-      case '^':
-        freq=466.2;
-        break;
-      case '$':
-        freq=493.9;
-        break;
-         
-    }
-    
-
-    
-    Serial.write("On joue la note\n");    
-    gh.setParamValue("freq", freq);
-    gh.setParamValue("gate", 1);
-    delay(600);
-    gh.setParamValue("gate", 0);
-    delay(100);
-  }
 }
 
 void loop() {
@@ -139,7 +72,101 @@ void loop() {
       }
     }
     if(clavier){
-      joueClavier();
+      if (Serial.available() > 0) {
+        // read the oldest byte in the serial buffer:
+        incomingByte = Serial.read();
+        switch(incomingByte){
+          case 'a':
+            freq=261.6;
+            break;
+          case 'z':
+            freq=277.2;
+            break;
+          case 'e':
+            freq=293.7;
+            break;
+          case 'r':
+            freq=311.1;
+            break;
+          case 't':
+            freq=329.6;
+            break;
+          case 'y':
+            freq=349.2;
+            break;
+          case 'u':
+            freq=370;
+            break;
+          case 'i':
+            freq=392;
+            break;
+          case 'o':
+            freq=415.3;
+            break;
+          case 'p':
+            freq=440;
+            break;
+          case '^':
+            freq=466.2;
+            break;
+          case '$':
+            freq=493.9;
+            break;
+          case '*':
+            freq=131;
+            break;
+              
+          case 'q':
+            freq=139;
+            break;
+              
+          case 's':
+            freq=147;
+            break;
+              
+          case 'd':
+            freq=156;
+            break;
+              
+          case 'f':
+            freq=165;
+            break;
+              
+          case 'g':
+            freq=175;
+            break;
+              
+          case 'h':
+            freq=185;
+            break;
+              
+          case 'j':
+            freq=196;
+            break;
+              
+          case 'k':
+            freq=208;
+            break;
+              
+          case 'l':
+            freq=220;
+            break;
+              
+          case 'm':
+            freq=233;
+            break;
+              
+          case '!':
+            freq=247;
+            break;
+        }
+            
+        gh.setParamValue("freq", freq);
+        gh.setParamValue("gate", 1);
+        delay(600);
+        gh.setParamValue("gate", 0);
+        delay(100);
+      }
     }
     else if (notefreq.available() ) {
       float note = notefreq.read();
@@ -147,7 +174,7 @@ void loop() {
       if(aap1.available()){
         float peak=aap1.read();
         float ecart = note_prec*0.02973;
-        if((note>note_prec+ecart or note<note_prec-ecart)and note>99 ){
+        if((note>note_prec+ecart or note<note_prec-ecart)and note>99 and note<1001){
           if(peak>threshold){
             Serial.printf("Note: %3.2f | Probability: %.2f, peak:%.2f\n", note, prob,peak);
             gh.setParamValue("freq", note);
@@ -160,7 +187,7 @@ void loop() {
             note_prec=note;
             prevPeak=0.8*peak;
           }
-        }else if (peak>=prevPeak and note>99){
+        }else if (peak>=prevPeak and note>99 and note<1001){
             
             Serial.printf("La meme Note: %3.2f | Probability: %.2f, peak:%.2f\n", note, prob,peak);
             gh.setParamValue("freq", note);
@@ -183,7 +210,7 @@ void loop() {
           float ecart = note_prec*0.02973;
           float note = notefreq.read();
           float prob = notefreq.probability();
-          if((note>note_prec+ecart or note<note_prec-ecart)and note>99){
+          if((note>note_prec+ecart or note<note_prec-ecart)and note>99 and note<1001){
             if(peak>threshold){
               Serial.printf("Note: %3.2f | Probability: %.2f, peak:%.2f\n", note, prob,peak);
               gh.setParamValue("freq", note);
@@ -196,7 +223,7 @@ void loop() {
               note_prec=note;
               prevPeak=peak;
             }
-          }else if(peak>=prevPeak and note>99){
+          }else if(peak>=prevPeak and note>99 and note<1001){
             Serial.printf("Note: %3.2f | Probability: %.2f, peak:%.2f\n", note, prob,peak);
             gh.setParamValue("freq", note);
             float v =analogRead(A0)*0.0009775171;
